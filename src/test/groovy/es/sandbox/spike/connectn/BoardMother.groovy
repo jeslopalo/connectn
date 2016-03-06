dfdb5329e6caf5762d337043302d73a7a7003704
Jesús López Alonso <jeslopalo@sandbox.es>
2016-03-06 01:26:14 +0100

    Fix bug and add new methods

--- src/test/groovy/es/sandbox/spike/connectn/BoardMother.groovy
+++ src/test/groovy/es/sandbox/spike/connectn/BoardMother.groovy
@@ -17,8 +17,40 @@ final class BoardMother {
         return new Board(SIMPLEST_BOARD_COLUMNS, SIMPLEST_BOARD_ROWS, SIMPLEST_BOARD_CHIPS_TO_WIN)
     }
 
+    static Board simplestBoard(List<Integer> redPlays, List<Integer> yellowPlays) {
+        final Board board = simplestBoard();
+
+        playTheGame(board, redPlays, yellowPlays)
+
+        return board;
+    }
+
     static Board mediumSizedBoard() {
-        return new Board(MEDIUM_BOARD_COLUMNS, MEDIUM_BOARD_ROWS, MEDIUM_BOARD_CHIPS_TO_WIN);
+        return new Board(MEDIUM_BOARD_CHIPS_TO_WIN, MEDIUM_BOARD_COLUMNS, MEDIUM_BOARD_ROWS);
+    }
+
+    static Board mediumSizedBard(List<Integer> redPlays, List<Integer> yellowPlays) {
+        final Board board = mediumSizedBoard();
+
+        playTheGame(board, redPlays, yellowPlays);
+
+        return board;
+    }
+
+    public static void playTheGame(Board board, List<Integer> redPlays, List<Integer> yellowPlays) {
+        final Queue<Integer> reds = new LinkedList<>(redPlays);
+        final Queue<Integer> yellows = new LinkedList<>(yellowPlays);
+
+        for (int plays = 0; plays < Math.max(redPlays.size(), yellowPlays.size()); plays++) {
+
+            if (reds.peek() != null) {
+                board.put(Color.RED, reds.poll());
+            }
+
+            if (yellows.peek() != null) {
+                board.put(Color.RED, yellows.poll());
+            }
+        }
     }
 
     private BoardMother() {
